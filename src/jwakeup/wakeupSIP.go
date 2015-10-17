aa039b903b7b5735ea16a265a4ec2f8b0a8d6736
nicolaeandrei <an.nicolae@jacobs-university.de>
2015-10-17 16:33:10 +0200

    SIPcomit

--- src/jwakeup/wakeupSIP.go
+++ src/jwakeup/wakeupSIP.go
@@ -7,3 +7,34 @@
 
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"io/ioutil"
+)
+
+func Hindex(w http.ResponseWriter, r *http.Request) {
+	dat, err := ioutil.ReadFile("./www/index.html")
+	if (err!=nil) {
+
+	}
+	//check(err)
+	//fmt.Print(string(dat))
+
+	fmt.Fprintf(w, string(dat))
+}
+
+func (wH wakeupSIP) wSIPstart(port string, nChan chan string) {
+	fmt.Println("Starting HTTP server...")
+	wH.toMain = nChan
+	http.HandleFunc("/", Hindex)
+	http.ListenAndServe(port, nil)
+}
+func (wH wakeupSIP) wSIPstop() {
+	fmt.Println("Stopping HTTP server...")
+}
+
+type wakeupSIP struct {
+	toMain chan string
+	
+}
