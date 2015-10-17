6f910ae6454944c12249324fc754b68b23e14727
Mitescu George Dan <d.mitescu@jacobs-university.de>
2015-10-17 14:14:41 +0200

    Deleted unnecesary shit

--- src/jwakeup/wakeupSIP/wakeupSIP.go
+++
@@ -1,9 +0,0 @@
-// JWakeup
-// Copyright (c) 2015 
-// Mitescu George Dan <d.mitescu@jacobs-university.de>
-// Nicolae Andrei <an.nicolae@jacobs-university.de>
-// Frasineanu Catalin Vlad <v.frasineanu@jacobs-university.de>
-// Zamfir Andrei Vlad <v.zamfir@jacobs-university.de>
-
-package jwakeup
-
