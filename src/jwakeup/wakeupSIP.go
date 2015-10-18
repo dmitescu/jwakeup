cacbf14be87032213986404fdbc46d71fac22444
nicolaeandrei <an.nicolae@jacobs-university.de>
2015-10-18 10:44:47 +0200

    muie

--- src/jwakeup/wakeupSIP.go
+++ src/jwakeup/wakeupSIP.go
@@ -24,8 +24,60 @@ func (wS *wakeupSIP) addCALL(nCall wCall){
 	ioutil.WriteFile("../../userbase/wakelist.xml", tempout, 0644)
 }
 
-func (wS *wakeupSIP) makeCALL(){
+func (wS *wakeupSIP) makeCALL(scall wCall){
 	
+	var INVITE_PACK SIPpacket 
+	INVITE_PACK.Type = "INVITE"
+	INVITE_PACK.Branch = "z9hG4bK-17409-1-0"
+	INVITE_PACK.call_id = "call_id"
+	INVITE_PACK.tag = "Call_Number"
+	INVITE_PACK.call_number = scall.Phonenr
+	INVITE_PACK.CsqmethodName = "INVITE"
+	INVITE_PACK.Csqint = "1"
+	INVITE_PACK.ContentType = "Wake up Call"
+	INVITE_PACK.ContentLength = "12"
+	INVITE_PACK.service = "service"
+	INVITE_PACK.remote_ip = "127.0.0.1"
+	INVITE_PACK.remote_port = "5060"
+	INVITE_PACK.transport = "UDP"
+	INVITE_PACK.local_ip = "127.0.0.1"
+	INVITE_PACK.local_port = "5061"
+
+	var ACK_PACK SIPpacket
+	ACK_PACK.Type = "INVITE"
+	ACK_PACK.Branch = "z9hG4bK-17409-1-0"
+	ACK_PACK.call_id = "call_id"
+	ACK_PACK.tag = "Call_Number"
+	ACK_PACK.call_number = scall.Phonenr
+	ACK_PACK.CsqmethodName = "INVITE"
+	ACK_PACK.Csqint = "1"
+	ACK_PACK.ContentLength = "12"
+	ACK_PACK.service = "service"
+	ACK_PACK.remote_ip = "127.0.0.1"
+	ACK_PACK.remote_port = "5060"
+	ACK_PACK.transport = "UDP"
+	ACK_PACK.local_ip = "127.0.0.1"
+	ACK_PACK.local_port = "5061"
+	ACK_PACK.peer_tag_param = "011"
+
+	var BYE_PACK SIPpacket
+	BYE_PACK.Type = "INVITE"
+	BYE_PACK.Branch = "z9hG4bK-17409-1-0"
+	BYE_PACK.call_id = "call_id"
+	BYE_PACK.tag = "Call_Number"
+	BYE_PACK.call_number = scall.Phonenr
+	BYE_PACK.CsqmethodName = "INVITE"
+	BYE_PACK.Csqint = "1"
+	BYE_PACK.ContentLength = "12"
+	BYE_PACK.service = "service"
+	BYE_PACK.remote_ip = "127.0.0.1"
+	BYE_PACK.remote_port = "5060"
+	BYE_PACK.transport = "UDP"
+	BYE_PACK.local_ip = "127.0.0.1"
+	BYE_PACK.local_port = "5061"
+	BYE_PACK.peer_tag_param = "011"
+
+
 	//wS.callOut.send()
 }
 
